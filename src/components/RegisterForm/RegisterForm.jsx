e816c374b01baa34b10e91d0f874eb47e3cbaa59
Cyrus Ducut <cyrus.ducut@fieldforce4.com>
2024-09-05 23:48:51 +0800

    Code cleanup

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,27 +1,12 @@
-import { useDispatch } from 'react-redux';
-import { register } from '../../redux/auth/authOperations';
 import css from './RegisterForm.module.css';
 import { AuthForm } from '../AuthForm/AuthForm';
 
 export const RegisterForm = () => {
-  const dispatch = useDispatch();
   const fields = [
     { name: 'name', type: 'text', placeholder: 'Name' },
     { name: 'email', type: 'email', placeholder: 'Email' },
     { name: 'password', type: 'password', placeholder: 'Password' },
   ];
-  const handleSubmit = e => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    dispatch(
-      register({
-        name: form.elements.name.value,
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      }),
-    );
-    form.reset();
-  };
 
   return (
     <div className={css.container}>
